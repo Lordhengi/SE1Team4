ad7f20a63dc8de1af7d3d1e46c1aa6e849ca41ef
Lordhengi <hendrik-doerr@web.de>
2017-10-20 23:00:56 +0200

    Erledigen von Aufgabe 2.3
    Hier sind in die Klasse ZeitTest die kritischen Problemfälle eingefügt
    worden, zum testen der neuen Klasse newZeit.
    newZeit ist die verbesserte Zeit Klasse.

--- Aufgaben/src/Aufgabe_02/ZeitTest.java
+++ Aufgaben/src/Aufgabe_02/ZeitTest.java
@@ -3,38 +3,47 @@ package Aufgabe_02;
 import org.junit.Before;
 import org.junit.Test;
 
+import sun.security.util.Debug;
+
 import static org.junit.Assert.assertEquals;
 
 public class ZeitTest {
 
   Zeit z01, z02;
+//  newZeit z03, z04; 
 
   @Before
   public void setUp() throws Exception {
     z01   = new Zeit(00.01f);
     z02   = new Zeit(00.02f);
+//    z03   = new newZeit(00,61);
+//    z04 = new newZeit(00,30);
   }
 
   @Test
   public void testStunden() {
     assertEquals(0, z01.stunden());
     assertEquals(0, z02.stunden());
+//    assertEquals(1, z03.stunden());
   }
 
   @Test
   public void testMinuten() {
     assertEquals(1, z01.minuten());
     assertEquals(2, z02.minuten());
+//    assertEquals(1, z03.minuten());
   }
 
   @Test
   public void testAdd() {
+//	z04.add(new newZeit(00,40)); assertEquals("01:10", z04.toString());
     z01.add(z01); assertEquals(z01, z01);
   }
 
   @Test
   public void testToString() {
     assertEquals("00:01", z01.toString());
+//	  assertEquals("01:01", z03.toString());
   }
 
 }
---
+++ Aufgaben/src/Aufgabe_02/newZeit.java
@@ -0,0 +1,44 @@
+package Aufgabe_02;
+
+import java.time.LocalTime;
+
+public class newZeit {
+	private LocalTime time = null;
+
+	public newZeit(int hours, int min) {
+		time = LocalTime.of(0, 0);
+		add(hours, min);
+	}
+
+	public int stunden() {
+		return this.time.getHour();
+	}
+
+	public int minuten() {
+		return this.time.getMinute();
+	}
+
+	public void add(newZeit other) {
+		LocalTime newTimeMin = time.plusMinutes(other.minuten());
+		LocalTime newTimeHoures = newTimeMin.plusHours(other.stunden());
+		time = newTimeHoures;
+	}
+
+	public void add(int hours, int min) {
+		LocalTime newTimeMin = time.plusMinutes(min);
+		LocalTime newTimeHoures = newTimeMin.plusHours(hours);
+		time = newTimeHoures;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%02d:%02d", stunden(), minuten());
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		if (other.getClass() != newZeit.class)
+			return false;
+		return this.time.equals(((newZeit) other).time);
+	}
+}

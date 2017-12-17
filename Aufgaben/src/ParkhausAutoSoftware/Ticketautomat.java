e7fac9b08d4e43c50e7a829d829f501049e0c651
dreima2s <32843900+dreima2s@users.noreply.github.com>
2017-12-17 17:23:07 +0100

    Ticketautomat

--- Aufgaben/src/ParkhausAutoSoftware/Ticketautomat.java
+++ Aufgaben/src/ParkhausAutoSoftware/Ticketautomat.java
@@ -1,6 +1,23 @@
 package ParkhausAutoSoftware;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ticketautomat {
 
-	private Ticket[] tickets;
+	private List<Ticket> tickets = new ArrayList<Ticket>();
+	int id;
+	
+	public Ticketautomat() {
+		id = 0;
+	}
+	
+	public void create() {
+		tickets.add(new Ticket(id, Manager.getpreis()));
+		id++;
+	}
+	
+	public List<Ticket> gettickets() {
+		return tickets;
+	}
 }
